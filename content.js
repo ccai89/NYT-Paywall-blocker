17be87451b8ba7d5bf0b3a6505db13a866511b89
ccai89 <ccai89@gmail.com>
2021-10-13 22:26:38 -0400

    removed ads from Google as well

--- content.js
+++ content.js
@@ -3,11 +3,9 @@ function ready() {
     document.getElementById('gateway-content').remove();
     setTimeout(document.querySelector('.css-mcm29f').setAttribute('style', 'position:relative'),1000);
     setTimeout(document.querySelector('.css-gx5sib').setAttribute('style', 'background: transparent'),1000);
-    setTimeout(document.getElementById('top-wrapper').remove(),1000);
-    setTimeout(document.getElementById('story-ad-1-wrapper').remove(),1000);
-    setTimeout(document.getElementById('story-ad-2-wrapper').remove(),1000);
-    setTimeout(document.getElementById('story-ad-3-wrapper').remove(),1000);
-    setTimeout(document.querySelectorAll(`div[id^='google']`).forEach(el => el.remove()),1500);
+    setTimeout(document.getElementById('top-wrapper').remove(),2500);
+    setTimeout(document.querySelectorAll(`div[id^='google']`).forEach(el => el.remove()),2500);
+    setTimeout(document.querySelectorAll(`div[id^='story-ad']`).forEach(el => el.remove()),2500);
     scroll(0,0);
   },1500);
 }
