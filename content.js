9a55469c5055b55aadd48dbff32d12f5e6b8b1c4
ccai89 <ccai89@gmail.com>
2021-10-13 21:39:22 -0400

    finished product

--- content.js
+++ content.js
@@ -1,4 +1,9 @@
-console.log("test");
+function ready() {
+  setTimeout(() => {
+    document.getElementById('gateway-content').remove();
+    setTimeout(document.querySelector('.css-mcm29f').setAttribute('style', 'position:relative'),500);
+    setTimeout(document.querySelector('.css-gx5sib').setAttribute('style', 'background: transparent'),500);
+  },1000);
+}
 
-let a = document.getElementById("gateway-content");
-console.log(a);
+document.addEventListener('DOMContentLoaded', ready);
