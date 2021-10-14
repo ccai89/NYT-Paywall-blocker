b2f51bfc78dee851f5f574aa81aac679a6a68a74
ccai89 <ccai89@gmail.com>
2021-10-14 17:49:20 -0400

    working icons

--- content.js
+++ content.js
@@ -1,13 +1,10 @@
 function ready() {
-  setTimeout(() => {
     document.getElementById('gateway-content').remove();
-    setTimeout(document.querySelector('.css-mcm29f').setAttribute('style', 'position:relative'),1000);
-    setTimeout(document.querySelector('.css-gx5sib').setAttribute('style', 'background: transparent'),1000);
-    setTimeout(document.getElementById('top-wrapper').remove(),2500);
-    setTimeout(document.querySelectorAll(`div[id^='google']`).forEach(el => el.remove()),2500);
-    setTimeout(document.querySelectorAll(`div[id^='story-ad']`).forEach(el => el.remove()),2500);
+    document.querySelector('.css-mcm29f').setAttribute('style', 'position:relative');
+    document.querySelector('.css-gx5sib').setAttribute('style', 'background: transparent');
+    document.getElementById('top-wrapper').remove();
+    document.querySelectorAll(`div[id^='google']`).forEach(el => el.remove());
+    document.querySelectorAll(`div[id^='story-ad']`).forEach(el => el.remove());
     scroll(0,0);
-  },1500);
 }
-
-document.addEventListener('DOMContentLoaded', ready);
+window.addEventListener('load', ready);
